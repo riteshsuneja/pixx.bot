52a631382c98e064227322a61821f472904ee465
riteshsuneja <ritesh16000suneja@gmail.com>
2026-02-12 02:37:43 +0000

    Refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Load Header
-    fetch('components/header.html')
+    fetch('/components/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
         });
 
     // Load Footer
-    fetch('components/footer.html')
+    fetch('/components/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-placeholder').innerHTML = data;
