380255a572c8117b2c62c62026565ef6fbe337f6
Avinash Karhana <avinashkarhana1@gmail.com>
2026-03-05 08:42:59 +0000

    Changed Solutions to fit better requirements

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
+            // Find what page we're on and decide if we need to show links depending on the current page
+            const currentPage = window.location.pathname;
+            const solutionsLink = document.getElementById('solutions-link');
+            const technologyLink = document.getElementById('technology-link');
+            const contactLink = document.getElementById('contact-link');
+
+            if (currentPage === '/index.html') {
+                solutionsLink.style.display = 'block';
+                technologyLink.style.display = 'block';
+                contactLink.style.display = 'block';
+            } else {
+                solutionsLink.style.display = 'none';
+                technologyLink.style.display = 'none';
+                contactLink.style.display = 'block';
+            }
         });
 
     // Load Footer
